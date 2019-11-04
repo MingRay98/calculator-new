71647d991003e84a3f3fe1682408f402bdf13644
Your Name <you@example.com>
2019-11-05 02:05:39 +0800

    Updates

---
+++ precache-manifest.616238f1e55febabe18469b171400973.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba97371cd856a5d3d3ee77986ddc6db7",
+    "url": "/calculator-new/index.html"
+  },
+  {
+    "revision": "fd857ba7f7c7811dd37f",
+    "url": "/calculator-new/static/css/main.c4dfb006.chunk.css"
+  },
+  {
+    "revision": "a97f17191942ae7b8fe9",
+    "url": "/calculator-new/static/js/2.e6bfa12b.chunk.js"
+  },
+  {
+    "revision": "fd857ba7f7c7811dd37f",
+    "url": "/calculator-new/static/js/main.2be6b6be.chunk.js"
+  },
+  {
+    "revision": "a6badb87a690c03460e0",
+    "url": "/calculator-new/static/js/runtime-main.d019e524.js"
+  }
+]);
